ad5d33a2e1a60b5f9053bdfea5103f0c749587dd
Edward Lopez <edtolo92@gmail.com>
2023-04-26 02:59:26 -0700

    updated mobile

--- app.js
+++ app.js
@@ -1,15 +1,37 @@
 let goku = document.getElementById("goku");
 let freeza = document.getElementById("freeza");
+let msgContainer = document.querySelector(".message-container");
 let counter = document.getElementById("counter");
 let gameover = document.getElementById("gameover");
 let button = document.getElementById("startover");
+let modal = document.querySelector(".victory");
+let audio = document.querySelector(".audio");
+let defeatAudio = document.querySelector(".defeated");
+let successAudio = document.querySelector(".success");
+let showCredits = document.querySelector(".credits");
+
 let jumpCounter = 0;
 
 function jump() {
   if (goku.classList !== "animate") {
     goku.classList.add("animate");
     jumpCounter++;
+
     counter.innerText = `You've jumped: ${jumpCounter}`;
+    if (jumpCounter > 3) {
+      audio.pause();
+
+      modal.classList.add("show");
+      freeza.remove();
+      successAudio.play();
+
+      setTimeout(() => {
+        goku.remove();
+        msgContainer.remove();
+
+        showCredits.classList.toggle("show-credits");
+      }, 2000);
+    }
   }
 
   setTimeout(() => {
@@ -33,9 +55,16 @@ let checkDead = setInterval(() => {
 
     if (gameover) {
       counter = counter.value;
+      audio.pause();
+      defeatAudio.play();
+    }
+
+    if (jumpCounter > 3) {
+      counter = counter.value;
     }
 
     gameover.classList.add("active");
     button.classList.add("show-btn");
+    jumpCounter.classList.remove();
   }
 }, 100);
